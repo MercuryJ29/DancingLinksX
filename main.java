19d74fd8e12354680efe8b066fee0a58218fdca7
MercuryJ29 <48653806+MercuryJ29@users.noreply.github.com>
2021-01-31 19:58:52 +0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+import bean.*;
+
+public class main {
+	public static void main(String[] args) {
+		DLX dlx=new DLX();
+		//dlx.inputDLX();
+		//dlx.testConstruction();
+		dlx.execute();
+	}
+}
+
